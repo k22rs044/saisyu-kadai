27f73d57344ad92b52c3c2b19b21dc0396ef14e5
k22rs044 <k22rs044@st.kyusan-u.ac.jp>
2023-12-07 10:48:21 +0900

    Project's Initial Commit

---
+++ www/keys.js
@@ -0,0 +1,3 @@
+// This is a mBaaS keys file
+let appKey="3f5f1c25deb7bbe9dacb7882d5259bab1cbd4bd0a236f5dc0477379ad26b20f4";
+let clientKey="a4cd00152e5e925bead34dd25e2c60c265a27ba9fd8d8203d8b069f2d1057373";
